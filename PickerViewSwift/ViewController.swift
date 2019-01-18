e1d6bad10d9a1c193cc50a7462d40c44856f904d
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-18 10:41:05 +0530

    PickerView_Swift
    Picker View

--- PickerViewSwift/ViewController.swift
+++ PickerViewSwift/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
 
+    @IBOutlet weak var txt: UITextField!
+    
+    
+    
+    var arr = [String]();
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        arr=["Asp.Net","iPhone","Android","Java","Designing"]
+        
+        //pickr.isHidden=false;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return arr.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return arr[row];
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        txt.text=arr[row];
     }
 
 
